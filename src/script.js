69aee84e72fd90ef708e4650649482aa4f33400c
shiki-s <93466662+shiki-s@users.noreply.github.com>
2021-11-29 20:38:29 +1100

    Added files
    Added:
    - index.html
    - script.js
    - style.css

---
+++ src/script.js
@@ -0,0 +1,68 @@
+function showTime() {
+  let now = new Date();
+  let weekday = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = weekday[now.getDay()];
+  let hour = now.getHours();
+  if (hour < 10) {
+    hour = `0${hour}`;
+  }
+  let minutes = now.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  let date = `${day}, ${hour}:${minutes}`;
+  let dateDisplay = document.querySelector("#today");
+  dateDisplay.innerHTML = date;
+}
+function displayWeather(response) {
+  let temp = Math.round(response.data.main.temp);
+  let headTemp = document.querySelector(".head-temperature");
+  headTemp.innerHTML = temp;
+}
+
+function currentLocationTemp(position) {
+  console.log(position);
+  let lat = position.coords.latitude;
+  let lon = position.coords.longitude;
+  console.log(lat);
+  console.log(lon);
+  let apiKey = "ba73db419b4c99c0a6fa92bf9033b20d";
+  let apiRoot = "https://api.openweathermap.org/data/2.5/weather";
+  let units = "metric";
+  let apiUrl = `${apiRoot}?lat=${lat}&lon=${lon}&appid=${apiKey}&units=${units}`;
+  axios.get(apiUrl).then(displayWeather);
+}
+
+function currentLocationClick(event) {
+  navigator.geolocation.getCurrentPosition(currentLocationTemp);
+}
+
+function locationSearch(event) {
+  event.preventDefault();
+  let locationInput = document.querySelector("#location-form-input");
+  let location = locationInput.value;
+  let locationDisplay = document.querySelector("#location");
+  locationDisplay.innerHTML = `${location}`;
+
+  let apiKey = "ba73db419b4c99c0a6fa92bf9033b20d";
+  let apiRoot = "https://api.openweathermap.org/data/2.5/weather";
+  let units = "metric";
+  let apiUrl = `${apiRoot}?q=${location}&appid=${apiKey}&units=${units}`;
+  axios.get(apiUrl).then(displayWeather);
+}
+
+let search = document.querySelector("#location-form");
+search.addEventListener("submit", locationSearch);
+
+let currentLocationButton = document.getElementById("current-location-button");
+currentLocationButton.addEventListener("click", currentLocationClick);
+showTime();
